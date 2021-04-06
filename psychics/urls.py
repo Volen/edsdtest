6a8dcef88aed80888f97d8acc8c8aa298d6df3a5
Vasiliy Voyakin <vaulttx@yandex.ru>
2021-04-06 12:18:39 +0300

    Add new template and routes

--- psychics/urls.py
+++ psychics/urls.py
@@ -1,8 +1,9 @@
   
 from django.urls import path
 
-from .views import index
+from .views import index, get_guess
 
 urlpatterns = [
     path('', index, name='index'),
+    path('get_guess', get_guess, name='get_guess'),
 ]
--- psychics/views.py
+++ psychics/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+import random
 
-# Create your views here.
 def index(request):
     return render(request, 'psychics/index.html')
+
+def get_guess(request): 
+    first = random.randint(10, 99)
+    first_name = "Гуржиев"   
+    second = random.randint(10, 99)
+    second_name = "Угадайкин"
+    request.session.first = first
+    request.session.first_name = first_name    
+    request.session.second = second
+    request.session.second_name = second_name
+    return render(request, 'pcychics/guess.html')
