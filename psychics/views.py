2687b56017ae7468402d169f5ba85994af565e1b
Vasiliy Voyakin <vaulttx@yandex.ru>
2021-04-06 12:49:29 +0300

    Small template modifications

--- psychics/views.py
+++ psychics/views.py
@@ -9,8 +9,8 @@ def get_guess(request):
     first_name = "Гуржиев"   
     second = random.randint(10, 99)
     second_name = "Угадайкин"
-    request.session.first = first
-    request.session.first_name = first_name    
-    request.session.second = second
-    request.session.second_name = second_name
-    return render(request, 'pcychics/guess.html')
+    request.session['first'] = first
+    request.session['first_name'] = first_name    
+    request.session['second'] = second
+    request.session['second_name'] = second_name
+    return render(request, 'psychics/guess.html')
