1896710bc2f4aa08fd0cc7eeb610f8c22af3582b
Vasiliy Voyakin <vaulttx@yandex.ru>
2021-04-06 16:06:20 +0300

    Uppdate views

--- psychics/models.py
+++ psychics/models.py
@@ -1,2 +1,7 @@
 from django.db import models
 
+class Psychic:
+
+    def __init__(self, name, credibility=100):
+        self.name = name
+        self.credibility = credibility
--- psychics/views.py
+++ psychics/views.py
@@ -1,13 +1,17 @@
 import random
+from edsdtest.settings import PSYCHICS_NAMES
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
 from .forms import CorrectAnswerForm
 
-
 def index(request):
-    return render(request, 'psychics/index.html')
+    if 'user_history' in request.session:
+        user_history = request.session['user_history'].split(',')
+    else:    
+        user_history = []
+    return render(request, 'psychics/index.html', {'user_history': user_history})
 
 def get_guess(request): 
     if request.method == 'POST':
@@ -16,23 +20,41 @@ def get_guess(request):
             print(request)
             print(request.POST['correct_answer'])
             correct_answer = request.POST['correct_answer']
+            if "user_history" not in request.session:
+                request.session['user_history'] = str(correct_answer)
+            else:
+                request.session['user_history'] = request.session['user_history'] + ",{}".format(correct_answer)
             return HttpResponseRedirect(reverse('check', args=[correct_answer]))
     else:
-        if "Гурджиев" not in request.session:
-            request.session["Гурджиев"] = 100
-        if "Угадайкин" not in request.session:
-            request.session["Угадайкин"] = 100
-        first = random.randint(10, 99)
-        first_name = "Гурджиев"   
-        second = random.randint(10, 99)
-        second_name = "Угадайкин"
-        request.session['first'] = first
-        request.session['first_name'] = first_name    
-        request.session['second'] = second
-        request.session['second_name'] = second_name
+        print(PSYCHICS_NAMES)
+        if 'user_history' in request.session:
+            user_history = request.session['user_history'].split(',')
+        else:    
+            user_history = []
+
+        for name in PSYCHICS_NAMES:
+            creability_name = name + "_credibility"
+            if creability_name not in request.session:
+                request.session[creability_name] = 100
+        for name in PSYCHICS_NAMES:
+            name_history = name + "_history"
+            psychics_guess = random.randint(10, 99)
+            if name_history not in request.session:
+                request.session[name_history] = str(psychics_guess)
+            else:
+                request.session[name_history] = request.session[name_history] + ",{}".format(psychics_guess)
+        
+        #first = 
+        #first_name = "Гурджиев"   
+        #second = random.randint(10, 99)
+        #second_name = "Угадайкин"
+        #request.session['first'] = first
+        #request.session['first_name'] = first_name    
+        #request.session['second'] = second
+        #request.session['second_name'] = second_name
         form = CorrectAnswerForm()    
     
-    return render(request, 'psychics/guess.html', {'form': form})
+    return render(request, 'psychics/guess.html', {'form': form, 'user_history': user_history})
 
 def check(request, correct_answer):
     print('Hello', correct_answer)
